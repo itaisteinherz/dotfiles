f608021abaf1b2d8017e76e6da181a7687d7583d
Itai Steinherz <itaisteinherz@gmail.com>
2022-01-16 21:34:08 +0200

    Use pretty outputs for ls and grep

--- dot_shared.sh
+++ dot_shared.sh
@@ -8,15 +8,19 @@ eval "`fnm env`"
 export PATH=$HOME/.local/bin:$PATH
 
 # Set Vim to be the default editor
-export EDITOR=/usr/bin/vim
+export EDITOR=vim
 
 # Set up Linuxbrew
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# some ls aliases
+# Some ls aliases
+alias ls='ls --color=tty' # for pretty output
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -alh'
+
+# For pretty grep output
+alias grep='grep --color=auto'
 
 # WSL-related utilities
 source ~/.wsl.sh
